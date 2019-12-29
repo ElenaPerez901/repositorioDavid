424dde79b86524a3a23481dd9b1bf90f90bf1d0a
davidabadp <47812104+davidabadp@users.noreply.github.com>
2019-12-29 11:58:31 +0100

    Create Prueba1.c

---
+++ Prueba1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int num;
+
+  printf(" %d\n", 4);
+
+  //Atención: con scanf el nombre de la 
+  //variable debe ir precedido de &
+  scanf("%i", &3);
+
+  printf(" %i\n", 3);
+
+  return 0;
+}
+
+
+
